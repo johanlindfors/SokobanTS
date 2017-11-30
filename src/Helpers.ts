8f2c170519bb0d042d73b6fe6f5cd029e159a6c3
Johan Lindfors <johan.lindfors@coderox.se>
2017-11-30 08:22:13 +0100

    Even more refactoring

---
+++ src/Helpers.ts
@@ -0,0 +1,45 @@
+namespace Sokoban {
+    export namespace Helpers {
+        // need a recursive function to copy arrays, no need to reinvent the wheel so I got it here
+        // http://stackoverflow.com/questions/10941695/copy-an-arbitrary-n-dimensional-array-in-javascript 
+        export function copyArray(a){
+            let newArray = a.slice(0);
+                for(let i = newArray.length; i>0; i--){
+                if(newArray[i] instanceof Array){
+                    newArray[i] = copyArray(newArray[i]);	
+                }
+            }
+            return newArray;
+        }
+    
+        export function parse(input: string) : number[][] {
+            let result : number[][] = [];
+            let row: number = 0;
+            result[row] = [];
+            for (let i = 0, col = 0; i <= input.length - 1; i++, col++) {
+                switch(input[i]){
+                    case '|':
+                        result[++row] = [];
+                        col = -1;
+                        break;
+                    case '#':
+                        result[row][col] = 1;
+                        break;
+                    case '$':
+                        result[row][col] = 3;
+                        break;
+                    case '@':
+                        result[row][col] = 4;
+                        break;
+                    case '.':
+                        result[row][col] = 2;
+                        break;
+                    default:
+                        result[row][col] = 0;
+                        break;
+                }
+            }
+            return result;
+        }
+    }
+}
--- src/Levels/Level.ts
+++ src/Levels/Level.ts
@@ -1,5 +1,8 @@
 /// <reference path="../Objects/Crate.ts" />
 /// <reference path="../Objects/Player.ts" />
+/// <reference path="../Helpers.ts" />
+
+import Helpers = Sokoban.Helpers;
 
 namespace Sokoban {
 
@@ -8,24 +11,28 @@ namespace Sokoban {
     const SPOT = 2;
     const CRATE = 3;
     const PLAYER = 4;
+    const TILESIZE = 40;
 
     export class Level extends Phaser.State {
         level: number[][];
-        player: Sokoban.Player;
-        tileSize: number = 40;
+        player: Player;
         undoArray: Array<number[][]>;
-        crates: Sokoban.Crate[][];
+        crates: Crate[][];
         // Variables used to create groups. The fist group is called fixedGroup, it will contain
         // all non-moveable elements (everything but crates and player).
         // Then we add movingGroup which will contain moveable elements (crates and player)
-        fixedGroup;
-        movingGroup;
+        fixedGroup: Phaser.Group;
+        movingGroup: Phaser.Group;
 
-        init(level: number[][]){
-            this.level = level;
-            this.undoArray = new Array<number[][]>();
+        // init(level: number[][]){
+        //     this.level = level;
+        //     this.undoArray = new Array<number[][]>();
+        // }
+                
+        init(level: string){
+            this.level = Helpers.parse(level);
         }
-
+        
         goFullScreen(){
             this.game.scale.pageAlignHorizontally = true;
             this.game.scale.pageAlignVertically = true;
@@ -34,6 +41,7 @@ namespace Sokoban {
 
         create() {
             this.game.input.keyboard.addCallbacks(this, this.onDown);
+            this.undoArray = new Array<number[][]>();
             this.goFullScreen();
             this.drawLevel();
         }
@@ -51,59 +59,58 @@ namespace Sokoban {
         // function to move the player
         move(deltaX: number, deltaY: number){
             // if destination tile is walkable...
-            if(this.isWalkable(this.player.posX+deltaX,this.player.posY+deltaY)){
+            if(this.isWalkable(this.player.posX + deltaX,this.player.posY + deltaY)){
                 // push current situation in the undo array
-                this.undoArray.push(this.copyArray(this.level));
-                this.movePlayer(deltaX,deltaY);
-                return;
+                this.undoArray.push(Helpers.copyArray(this.level));
+                this.movePlayer(deltaX, deltaY);
             }
             // if the destination tile is a crate... 
-            if(this.isCrate(this.player.posX + deltaX, this.player.posY + deltaY)){
+            else if(this.isCrate(this.player.posX + deltaX, this.player.posY + deltaY)){
                 // ...if  after the create there's a walkable tils...
                 if(this.isWalkable(this.player.posX + 2 * deltaX, this.player.posY + 2 * deltaY)) {
                     // push current situation in the undo array
-                    this.undoArray.push(this.copyArray(this.level));
+                    this.undoArray.push(Helpers.copyArray(this.level));
                     // move the crate
-                    this.moveCrate(deltaX,deltaY);			  
+                    this.moveCrate(deltaX, deltaY);			  
                     // move the player	
-                    this.movePlayer(deltaX,deltaY);
+                    this.movePlayer(deltaX, deltaY);
                 }
             }
         }
 
-      	// function to move the crate
-        moveCrate(deltaX: number,deltaY: number) {
-            
-            var crate = this.crates[this.player.posY + deltaY][this.player.posX + deltaX];
-            crate.move(deltaX, deltaY, this.tileSize);
-
-            // moving with a 1/10s tween
-            var crateTween = this.game.add.tween(this.crates[this.player.posY + deltaY][this.player.posX + deltaX]);
-            crateTween.to({
-                x: this.crates[this.player.posY + deltaY][this.player.posX + deltaX].x + deltaX * this.tileSize,
-                y: this.crates[this.player.posY + deltaY][this.player.posX + deltaX].y + deltaY * this.tileSize,
-            }, 100, Phaser.Easing.Linear.None,true);
-            // updating crates array   
-            this.crates[this.player.posY + 2 * deltaY][this.player.posX + 2 * deltaX] = this.crates[this.player.posY + deltaY][this.player.posX + deltaX];
-            this.crates[this.player.posY + deltaY][this.player.posX + deltaX] = null;
-            // updating crate old position in level array  
-            this.level[this.player.posY + deltaY][this.player.posX + deltaX] -= CRATE;
-            // updating crate new position in level array  
-            this.level[this.player.posY + 2 * deltaY][this.player.posX + 2 * deltaX] += CRATE;
-            // changing crate frame accordingly  
-            this.crates[this.player.posY + 2 * deltaY][this.player.posX + 2 * deltaX].frame = this.level[this.player.posY + 2 * deltaY][this.player.posX + 2 * deltaX];
+        // function to move the player
+        movePlayer(deltaX: number, deltaY: number){
+            // updating player old position in level array   
+            this.level[this.player.posY][this.player.posX] -= PLAYER;  
+            // let the player move with tweening
+            this.player.move(deltaX, deltaY, TILESIZE);
+            // updating player new position in level array 
+            this.level[this.player.posY][this.player.posX] += PLAYER;  
+            // changing player frame accordingly  
+            this.player.frame = this.level[this.player.posY][this.player.posX];
         }
         
-        // need a recursive function to copy arrays, no need to reinvent the wheel so I got it here
-        // http://stackoverflow.com/questions/10941695/copy-an-arbitrary-n-dimensional-array-in-javascript 
-        copyArray(a){
-            var newArray = a.slice(0);
-                for(var i = newArray.length; i>0; i--){
-                if(newArray[i] instanceof Array){
-                    newArray[i] = this.copyArray(newArray[i]);	
-                }
-            }
-            return newArray;
+      	// function to move the crate
+        moveCrate(deltaX: number, deltaY: number) {
+            let oldCratePosX = this.player.posX + deltaX;
+            let oldCratePosY = this.player.posY + deltaY;
+
+            let newCratePosX = this.player.posX + 2 * deltaX;
+            let newCratePosY = this.player.posY + 2 * deltaY;
+            
+            let crate = this.crates[oldCratePosY][oldCratePosX];
+            crate.move(deltaX, deltaY, TILESIZE);
+
+            // updating crates array   
+            this.crates[newCratePosY][newCratePosX] = crate;
+            this.crates[oldCratePosY][oldCratePosX] = null;
+
+            // updating crate positions in level array  
+            this.level[oldCratePosY][oldCratePosX] -= CRATE;
+            this.level[newCratePosY][newCratePosX] += CRATE;
+
+            // changing crate frame accordingly  
+            crate.frame = this.level[newCratePosY][newCratePosX];
         }
 
         onDown(e) {
@@ -129,30 +136,18 @@ namespace Sokoban {
                     // undo
                     case 46:
                         // if there's something to undo...
-                        if(this.undoArray.length>0){
+                        if(this.undoArray.length > 0){
                             // then undo! and remove the latest move from undoArray
                             var undoLevel = this.undoArray.pop();
                             this.fixedGroup.destroy();
                             this.movingGroup.destroy();
                             this.level = [];
-                            this.level = this.copyArray(undoLevel);
+                            this.level = Helpers.copyArray(undoLevel);
                             this.drawLevel();
                         }
                         break;
                 }
             }
-        }
-
-        // function to move the player
-        movePlayer(deltaX: number,deltaY: number){
-            // updating player old position in level array   
-            this.level[this.player. posY][this.player.posX] -= PLAYER;  
-            // let the player move with tweening
-            this.player.move(deltaX, deltaY, this.tileSize);
-            // updating player new position in level array 
-            this.level[this.player.posY][this.player.posX] += PLAYER;  
-            // changing player frame accordingly  
-            this.player.frame = this.level[this.player.posY][this.player.posX];
         }
 
         drawLevel(){  
