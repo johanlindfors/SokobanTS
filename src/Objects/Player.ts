1d766edc6e19cba37f274c90623b2f7ce78f4788
Johan Lindfors <Johan.Lindfors@truesec.se>
2023-06-07 22:21:54 +0200

    Player now looking backward when moving up

--- src/Objects/Player.ts
+++ src/Objects/Player.ts
@@ -5,10 +5,12 @@ namespace Sokoban {
         posX: number;
         posY: number;
         isMoving: boolean;
+        isLookingForward: boolean;
         
         constructor(scene: Phaser.Scene, x: number, y: number) {
             super(scene, x, y, 'tiles', 3);
             this.isMoving = false;
+            this.isLookingForward = true;
         }
 
         move(deltaX: number, deltaY: number, tileSize: number){
--- src/Scenes/GamePlay.ts
+++ src/Scenes/GamePlay.ts
@@ -6,14 +6,14 @@
 import Helpers = Sokoban.Helpers;
 
 namespace Sokoban {
- 
+
     class LevelConfig {
         level: string
     }
 
     export class GamePlay extends Phaser.Scene {
         player: Player;
-        fixedGroup: Phaser.GameObjects.Group;        
+        fixedGroup: Phaser.GameObjects.Group;
         cursors: Phaser.Types.Input.Keyboard.CursorKeys;
         map: Map;
 
@@ -36,13 +36,13 @@ namespace Sokoban {
 
         // function to move the player
         move(deltaX: number, deltaY: number){
-            // if the destination tile is a crate... 
+            // if the destination tile is a crate...
             if(this.map.isCrate(this.player.posX + deltaX, this.player.posY + deltaY)){
                 // ...if  after the create there's a walkable tils...
                 if(this.map.isWalkable(this.player.posX + 2 * deltaX, this.player.posY + 2 * deltaY)) {
                     // move the crate
                     this.map.moveCrate(deltaX, deltaY, this.player.posX, this.player.posY);
-                    // move the player	
+                    // move the player
                     this.movePlayer(deltaX, deltaY);
                 }
             }
@@ -54,31 +54,39 @@ namespace Sokoban {
 
         // function to move the player
         movePlayer(deltaX: number, deltaY: number){
-            // updating player old position in level array   
-            this.map.level[this.player.posY][this.player.posX] -= PLAYER;  
+            // updating player old position in level array
+            this.map.level[this.player.posY][this.player.posX] -= PLAYER;
             // let the player move with tweening
             this.player.move(deltaX, deltaY, TILESIZE);
-            // updating player new position in level array 
-            this.map.level[this.player.posY][this.player.posX] += PLAYER;  
+            // updating player new position in level array
+            this.map.level[this.player.posY][this.player.posX] += PLAYER;
             // changing player frame accordingly
-            this.player.frame = this.textures.getFrame(
-                'tiles',
-                this.map.level[this.player.posY][this.player.posX]
-            );
+            // this.player.frame = this.textures.getFrame(
+            //     'tiles',
+            //     this.map.level[this.player.posY][this.player.posX]
+            // );
         }
-        
+
         update() {
             // if the player is not moving...
             if(!this.player.isMoving){
-                if(this.cursors.left.isDown){
+                if(this.cursors.left.isDown) {
+                    this.player.isLookingForward = true;
                     this.move(-1,0);
                 } else if(this.cursors.up.isDown) {
+                    this.player.isLookingForward = false;
                     this.move(0,-1);
                 } else if(this.cursors.right.isDown) {
+                    this.player.isLookingForward = true;
                     this.move(1,0);
                 } else if(this.cursors.down.isDown) {
+                    this.player.isLookingForward = true;
                     this.move(0,1);
                 }
+                this.player.frame = this.textures.getFrame(
+                    'tiles',
+                    this.player.isLookingForward ? PLAYER : PLAYER+SPOT
+                );
             }
         }
 
@@ -87,16 +95,16 @@ namespace Sokoban {
             for(let y = 0; y < this.map.level.length; y++){
                  // looping through all level columns
                 for(let x = 0; x < this.map.level[y].length; x++){
-                    if( this.map.level[y][x] == PLAYER || 
+                    if( this.map.level[y][x] == PLAYER ||
                         this.map.level[y][x] == PLAYER+SPOT ){
                         // player creation
                         this.player.x = TILESIZE * x;
                         this.player.y = TILESIZE * y;
                         // assigning the player the proper frame
-                        this.player.frame = this.textures.getFrame(
-                            'tiles',
-                            this.map.level[y][x]
-                        );
+                        // this.player.frame = this.textures.getFrame(
+                        //     'tiles',
+                        //     this.map.level[y][x]
+                        // );
                         // creation of two custom attributes to store player x and y position
                         this.player.posX = x;
                         this.player.posY = y;
